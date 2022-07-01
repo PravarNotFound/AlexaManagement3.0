2c185c491f2f06fc29a93f0148dc4b9d61c6de3f
PravarNotFound <105976832+PravarNotFound@users.noreply.github.com>
2022-07-01 09:38:53 +0530

    Update start.bat
    Signed-off-by: PravarNotFound <105976832+PravarNotFound@users.noreply.github.com>

--- start.bat
+++ start.bat
@@ -1,6 +1,6 @@
 # why I'm changing this 
 
 @echo off
-TITLE RocksAlexaRobot Chan
+TITLE OliviaCuteBot Chan
 :: Enables virtual env mode and then starts Alexa
 env\scripts\activate.bat && py -m RocksAlexaRobot
